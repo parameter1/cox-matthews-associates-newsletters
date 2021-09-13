6e2d115d096152c403e5d7d2abcd063dcc13df93
Jessica Wade <jessica.wade@parameter1.com>
2021-09-13 13:33:45 -0500

    Initial setup of config files

---
+++ tenants/all/config/brands.js
@@ -0,0 +1,36 @@
+module.exports = {
+  diverse: {
+    brandsName: 'Diverse',
+    logoSrc: '',
+    subscribeLink: '',
+    contactLinks: [
+      { href: '', label: '' },
+    ],
+    socialMedia: {
+      imagePath: '',
+      links: [
+        { provider: 'facebook', href: '', target: '_blank' },
+        { provider: 'twitter', href: '', target: '_blank' },
+        { provider: 'linkedin', href: '', target: '_blank' },
+        { provider: 'youtube', href: '', target: '_blank' },
+      ],
+    },
+  },
+  ccnews: {
+    brandName: 'CCNews',
+    logoSrc: '',
+    subscribeLink: '',
+    contactLinks: [
+      { href: '', label: '' },
+    ],
+    socialMedia: {
+      imagePath: '',
+      links: [
+        { provider: 'facebook', href: '', target: '_blank' },
+        { provider: 'twitter', href: '', target: '_blank' },
+        { provider: 'linkedin', href: '', target: '_blank' },
+        { provider: 'youtube', href: '', target: '_blank' },
+      ],
+    },
+  },
+};
