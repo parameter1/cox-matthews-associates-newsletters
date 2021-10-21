4f2306179b86a77ca2492545edd4872f95a6b0c9
Jessica Wade <jessica.wade@parameter1.com>
2021-10-21 12:31:25 -0500

    Add emailx ad slots ccnews

--- tenants/all/config/email-x.js
+++ tenants/all/config/email-x.js
@@ -3,8 +3,31 @@ const EmailXConfiguration = require('@parameter1/base-cms-marko-newsletters-emai
 const config = new EmailXConfiguration(process.env.EMAILX_SERVE_URI || 'https://diverse.serve.email-x.parameter1.com');
 
 config
-  .setAdUnits('', [
-
+  .setAdUnits('ccnews-now', [
+    {
+      name: 'ad-slot-1',
+      id: '61718f6f0b81fa1a4a8068bb',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'ad-slot-2',
+      id: '61718f82c9a53c50cec6c153',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'ad-slot-3',
+      id: '61718f970b81fa1a098068e0',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'ad-slot-4',
+      id: '61718fa8c9a53cb0a2c6c166',
+      width: 600,
+      height: 100,
+    },
   ]);
 
 module.exports = config;
