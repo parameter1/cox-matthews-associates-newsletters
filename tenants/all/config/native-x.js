f6652147f8c7846199e6d1ee811ff3a83a39bc03
Brian Miller <brian@parameter1.com>
2023-08-11 07:46:23 -0500

    NativeX Config & Fetch calls utilizing core package
    These along with the base-cms package upgrade/depchanges will make configuration and fetching elements shared within core pacakge.

--- tenants/all/config/native-x.js
+++ tenants/all/config/native-x.js
@@ -1,23 +1,25 @@
-module.exports = {
-  uri: 'https://diverse.native-x.parameter1.com',
-  placements: {
-    'ccnews-now': {
-      'native-slot-1': '617190722f211d0001c095dc',
-    },
-    'diverse-daily': {
-      'native-slot-1': '6176b70d2f211d0001c099db',
-    },
-    'diverse-health': {
-      'native-slot-1': '617813f92f211d0001c09a76',
-    },
-    'diverse-hiring': {
-      'native-slot-1': '6178153d2175830001464af8',
-    },
-    'diverse-military': {
-      'native-slot-1': '617816d22175830001464b39',
-    },
-    'diverse-weekly-recap': {
-      'native-slot-1': '6176ba2a2f211d0001c09a00',
-    },
-  },
-};
+const NativeXConfiguration = require('@parameter1/base-cms-marko-newsletters-native-x/config');
+
+const config = new NativeXConfiguration('https://diverse.native-x.parameter1.com/email-placement', { enabled: true });
+
+config
+  .setAliasPlacements('ccnews-now', [
+    { name: 'native-slot-1', id: '617190722f211d0001c095dc' },
+  ])
+  .setAliasPlacements('diverse-daily', [
+    { name: 'native-slot-1', id: '6176b70d2f211d0001c099db' },
+  ])
+  .setAliasPlacements('diverse-health', [
+    { name: 'native-slot-1', id: '617813f92f211d0001c09a76' },
+  ])
+  .setAliasPlacements('diverse-hiring', [
+    { name: 'native-slot-1', id: '6178153d2175830001464af8' },
+  ])
+  .setAliasPlacements('diverse-military', [
+    { name: 'native-slot-1', id: '617816d22175830001464b39' },
+  ])
+  .setAliasPlacements('diverse-weekly-recap', [
+    { name: 'native-slot-1', id: '6176ba2a2f211d0001c09a00' },
+  ]);
+
+module.exports = config;
